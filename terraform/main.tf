60da972ba977f78dcdbd7287d960afe338578839
Raphael Fernandes Pereira <raphaelfp@users.noreply.github.com>
2019-09-07 22:19:13 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
-provider "aws" {
-  region = "us-west-X"
+module "docker" {
+    source = "./docker"
+    
+    image = "ghost:2-alpine"
+    app_name =  "ghost_blog"
+    ports = {
+        internal = 2368
+        external = 80
+    }
 }
